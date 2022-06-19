84ccbe0c82bab309df405a1e5000aa0c0cf4a31a
satishk8s <83827462+satishk8s@users.noreply.github.com>
2022-06-19 22:06:15 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+// https://vitejs.dev/guide/build.html#public-base-path
+
+export default defineConfig({
+  plugins: [vue({
+    preprocessStyles: true
+  })],
+  css: {
+    preprocessorOptions: {
+      scss: {}
+    }
+  },
+  base: './'
+});
